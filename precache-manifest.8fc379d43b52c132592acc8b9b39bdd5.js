92c22e58a9562c5c20e9d787528a60a15740fa7d
koanleach <m.martisca@gmail.com>
2020-05-19 16:55:51 +0200

    Updates

---
+++ precache-manifest.8fc379d43b52c132592acc8b9b39bdd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f575a373f4b32fb514045d859a0635",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b04af0975bbf58f2d4d6",
+    "url": "/monsters-rolodex/static/css/main.629da523.chunk.css"
+  },
+  {
+    "revision": "75388fd1a313f736f3e8",
+    "url": "/monsters-rolodex/static/js/2.fcd26ab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fcd26ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b04af0975bbf58f2d4d6",
+    "url": "/monsters-rolodex/static/js/main.c04acd19.chunk.js"
+  },
+  {
+    "revision": "a4c9c2237d7843219f81",
+    "url": "/monsters-rolodex/static/js/runtime-main.871502de.js"
+  }
+]);
